db60e9fcbc3bf779d7f89fbcee59957cacb44896
Nate Pope <nspope@utexas.edu>
2022-10-19 11:32:20 -0700

    Bed to intervals

--- data/masks/reformat.sh
+++ data/masks/reformat.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 for BED_RAW in *.mask.bed; do
   CHROM=`basename $BED_RAW | perl -pe 's/(epru...).+/$1/g'`
-  cat $BED_RAW >eucera_pruinosa.$CHROM.mask.bed
+  cut -f2-3 $BED_RAW >eucera_pruinosa.$CHROM.mask
 done
 
-tar czvf ../eucera_pruinosa.mask.tar.gz eucera_pruinosa.*.mask.bed
-rm eucera_pruinosa.*.mask.bed
+tar czvf ../eucera_pruinosa.mask.tar.gz eucera_pruinosa.*.mask
+rm eucera_pruinosa.*.mask
