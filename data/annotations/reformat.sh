9f22aebae5e5b43fa9c8165f43cc1d0dbde6bf15
Nate Pope <nspope@utexas.edu>
2022-10-19 11:46:55 -0700

    Change annotation format

--- data/annotations/reformat.sh
+++ data/annotations/reformat.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 for BED_RAW in *.exons.bed; do
   CHROM=`basename $BED_RAW | perl -pe 's/(epru...).+/$1/g'`
-  cut -f1-3 $BED_RAW >eucera_pruinosa.$CHROM.exons.bed
+  cut -f2-3 $BED_RAW >eucera_pruinosa.$CHROM.exons
 done
 
-tar czvf ../eucera_pruinosa.annotation.tar.gz eucera_pruinosa.*.exons.bed
-rm eucera_pruinosa.*.exons.bed
+tar czvf ../eucera_pruinosa.annotation.tar.gz eucera_pruinosa.*.exons
+rm eucera_pruinosa.*.exons
 
 cat epru.annotation.gff3.gz >../eucera_pruinosa.annotation.gff3.gz
--- data/genetic_maps/reformat.sh
+++ data/genetic_maps/reformat.sh
@@ -5,7 +5,13 @@ for HAPMAP_RAW in *.hapmap; do
   NR==1 {print "Chromosome\tPosition(bp)\tRate(cM/Mb)\tMap(cM)"}
   NR>1 {print chr"\t"$1"\t"$2"\t"$3}
   ' $HAPMAP_RAW >eucera_pruinosa.$CHROM.hapmap.txt
-done
 
-tar czvf ../eucera_pruinosa.genetic_map.tar.gz eucera_pruinosa.*.hapmap.txt
+  echo "
+import msprime
+map = msprime.RateMap.read_hapmap('eucera_pruinosa.$CHROM.hapmap.txt')
+print('$CHROM', map.mean_rate)
+" | python3
+done >average_rates.txt
+
+#tar czvf ../eucera_pruinosa.genetic_map.tar.gz eucera_pruinosa.*.hapmap.txt
 rm eucera_pruinosa.*.hapmap.txt
