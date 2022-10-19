f122953c4ac77050ec40053223f509b1c8703f79
Nate Pope <nspope@utexas.edu>
2022-10-19 11:53:50 -0700

    Remove 1bp intervals in mask

--- data/masks/reformat.sh
+++ data/masks/reformat.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 for BED_RAW in *.mask.bed; do
   CHROM=`basename $BED_RAW | perl -pe 's/(epru...).+/$1/g'`
-  cut -f2-3 $BED_RAW >eucera_pruinosa.$CHROM.mask
+  #only retain masked intervals >=1 Kb
+  awk '$3-$2 >= 1000 {print $2"\t"$3}' $BED_RAW >eucera_pruinosa.$CHROM.mask
 done
 
 tar czvf ../eucera_pruinosa.mask.tar.gz eucera_pruinosa.*.mask
